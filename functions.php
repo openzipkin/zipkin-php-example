95d1ae9a2a5334fced415a227042754c6a6383cf
José Carlos Chávez <jcchavezs@gmail.com>
2019-08-04 13:35:13 +0200

    docs: improves documentation in the create_tracing function.

--- functions.php
+++ functions.php
@@ -4,14 +4,19 @@ use Zipkin\Endpoint;
 use Zipkin\Samplers\BinarySampler;
 use Zipkin\TracingBuilder;
 
-function create_tracing($endpointName, $ipv4)
+/**
+ * create_tracing function is a handy function that allows you to create a tracing
+ * component by just passing the local service information. If you need to pass a 
+ * custom zipkin server URL use the HTTP_REPORTER_URL env var.
+ */
+function create_tracing($localServiceName, $localServiceIPv4, $localServicePort = null)
 {
     $httpReporterURL = getenv('HTTP_REPORTER_URL');
     if ($httpReporterURL === false) {
         $httpReporterURL = 'http://localhost:9411/api/v2/spans';
     }
 
-    $endpoint = Endpoint::create($endpointName, $ipv4, null, 2555);
+    $endpoint = Endpoint::create($localServiceName, $localServiceIPv4, null, $localServicePort);
 
     /* Do not copy this logger into production.
      * Read https://github.com/Seldaek/monolog/blob/master/doc/01-usage.md#log-levels
